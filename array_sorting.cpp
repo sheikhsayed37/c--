d2ae9b787302b1866cfb41982c153d3503ef6df1
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-12-18 11:47:35 +0600

    1

--- array_sorting.cpp
+++ array_sorting.cpp
@@ -5,6 +5,7 @@ int main()
  int n;
  cin>> n;
  int a[n];
+
  for(int i=0;i<n;i++)
  {
     cin>> a[i];
@@ -13,6 +14,7 @@ int main()
  sort(a,a+4);     // sort moloto asending decending er jonno use korte hoi
                 // a[0] teke a[4] porjonto ascending korse bakita teke gese or jaigai
                  //4 5 2 8 agola ashcending hoise bakita teke gese
+                 
  for(int i=0;i<n;i++)
  {
     cout << a[i]<< " ";
--- constructor_fun.cpp
+++ constructor_fun.cpp
@@ -9,12 +9,14 @@ class student
     double gpa;
     student(int r,int c,double g) // moloto onk student er 
     //name dia easily tar name sob declare korte prba
+    
     {
         roll=r;
         cls=c;
         gpa=g; 
     }
 };
+
 student fun()
 {
     student sayed(45,13,5.00);
